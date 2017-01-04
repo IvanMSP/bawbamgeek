523717f91f664e1764ae9772705a9fcfb0aa5a4e
IvanMSP <ivanlopz11@gmail.com>
2017-01-04 00:54:50 -0600

    Bawbamgeek site

---
+++ Bawbamgeek/urls.py
@@ -0,0 +1,15 @@
+
+from django.conf.urls import url,include
+from django.contrib import admin
+from django.views.static import serve
+from django.conf import settings
+from main import urls as UrlsMain
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^',include(UrlsMain,namespace="home")),
+    url(
+    	regex=r'^media/(?P<path>.*)$',
+    	view=serve,
+    	kwargs={'document_root':settings.MEDIA_ROOT}),
+]
